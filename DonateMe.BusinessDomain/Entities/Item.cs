5111105a637da297745ccf134f86853107073f73
me <dmcliver@gmail.com>
2015-09-22 13:20:25 +1200

    Added Item entity

---
+++ DonateMe.BusinessDomain/Entities/Item.cs
@@ -0,0 +1,17 @@
+﻿namespace DonateMe.BusinessDomain.Entities
+{
+    public class Item
+    {
+        public Item(string name)
+        {
+            Name = name;
+        }
+
+        protected Item(){}
+
+        public int ItemId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; set; }
+        public string Model { get; set; }
+    }
+}
