e0f6abd5e7085a99677001dfd8237f47803ed448
me <dmcliver@gmail.com>
2015-09-28 18:50:10 +1300

    Made HomeController tests a bit stronger

--- DonateMe.Web.Tests/HomeControllerTest.cs
+++ DonateMe.Web.Tests/HomeControllerTest.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Web.Mvc;
 using DonateMe.BusinessDomain.Entities;
 using DonateMe.DataLayer.Repositories;
 using DonateMe.Web.Controllers;
+using DonateMe.Web.Models;
 using NSubstitute;
 using NUnit.Framework;
+// ReSharper disable PossibleNullReferenceException
 
 namespace DonateMe.Web.Tests
 {
@@ -15,32 +18,41 @@ namespace DonateMe.Web.Tests
         public void GetChildren_WithChildrenFound_BuildsItemNodeModel()
         {
             Guid parentId = Guid.NewGuid();
-            var itemCategories = new List<ItemCategory>{new ItemCategory(Guid.NewGuid(), "Menu")};
+            var childCategories = new List<ItemCategory>{new ItemCategory(Guid.NewGuid(), "Menu")};
+            var expectedModel = new List<ItemNodeModel>();
 
-            IItemNodeModelBuilder itemNodeModelBuilder = Substitute.For<IItemNodeModelBuilder>();
-            IItemCategoryRelationRepository itemCategoryRelationRepository = Substitute.For<IItemCategoryRelationRepository>();
-            itemCategoryRelationRepository.GetChildCategoriesByParentId(parentId).Returns(itemCategories);
+            IItemNodeModelBuilder itemNodeModelBuilder = Mock.For<IItemNodeModelBuilder>();
+            itemNodeModelBuilder.Build(parentId, null, childCategories).Returns(expectedModel);
+            
+            IItemCategoryRelationRepository itemCategoryRelationRepository = Mock.For<IItemCategoryRelationRepository>();
+            itemCategoryRelationRepository.GetChildCategoriesByParentId(parentId).Returns(childCategories);
             itemCategoryRelationRepository.GetTopLevelCategories().Returns((IEnumerable<ItemCategory>) null);
 
             HomeController controller = new HomeController(itemCategoryRelationRepository, itemNodeModelBuilder);
-            controller.GetChildren(parentId);
+            var model = (controller.GetChildren(parentId) as ViewResult).Model as List<ItemNodeModel>;
 
-            itemNodeModelBuilder.Received(1).Build(parentId, null, itemCategories);
+            itemNodeModelBuilder.Received(1).Build(parentId, null, childCategories);
+            Assert.That(model, Is.SameAs(expectedModel));
         }
 
         [Test]
         public void GetChildren_WithNoChildrenFound_DoesntBuildItemNodeModel()
         {
             Guid parentId = Guid.NewGuid();
+            const string expectedNameOfFirstModel = "Parent";
 
-            IItemNodeModelBuilder itemNodeModelBuilder = Substitute.For<IItemNodeModelBuilder>();
-            IItemCategoryRelationRepository itemCategoryRelationRepository = Substitute.For<IItemCategoryRelationRepository>();
+            IItemNodeModelBuilder itemNodeModelBuilder = Mock.For<IItemNodeModelBuilder>();
+
+            IItemCategoryRelationRepository itemCategoryRelationRepository = Mock.For<IItemCategoryRelationRepository>();
             itemCategoryRelationRepository.GetChildCategoriesByParentId(parentId).Returns(new List<ItemCategory>());
+            itemCategoryRelationRepository.GetTopLevelCategories().Returns(new List<ItemCategory>{new ItemCategory(Guid.NewGuid(), expectedNameOfFirstModel)});
 
             HomeController controller = new HomeController(itemCategoryRelationRepository, itemNodeModelBuilder);
-            controller.GetChildren(parentId);
+            var model = (controller.GetChildren(parentId) as ViewResult).Model as List<ItemNodeModel>;
 
             itemNodeModelBuilder.DidNotReceiveWithAnyArgs().Build(Guid.Empty, null, null);
+            Assert.That(model.Count, Is.EqualTo(1));
+            Assert.That(model[0].Name, Is.EqualTo(expectedNameOfFirstModel));
         }
     }
 }
---
+++ DonateMe.Web.Tests/Mock.cs
@@ -0,0 +1,12 @@
+﻿using NSubstitute;
+
+namespace DonateMe.Web.Tests
+{
+    public static class Mock  
+    {
+        public static T For<T>() where T : class
+        {
+            return Substitute.For<T>();
+        }
+    }
+}
