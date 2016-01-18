312e79e85badcc71bb76e02d8a16d891e56755f5
dmcliver <dmcliver@gmail.com>
2016-01-18 18:32:34 +1300

    Added files that had been untracked for product upload page

---
+++ DonateMe/Controllers/ContributeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DonateMe.Web.Controllers
+{
+    public class ContributeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DonateMe/SinglePageAppScripts/ProductHomeController.js
@@ -0,0 +1,27 @@
+﻿app.controller("ProductHomeController", [
+
+    "$scope", "ProductRepository", function ($scope, productRepository) {
+
+        $scope.message = "";
+        $scope.info = null;
+
+        $scope.update = function (id) {
+            productRepository.getByCategoryId(id, $scope.onProductsReceived);
+        }
+
+        $scope.onProductsReceived = function (products) {
+
+            if (products.length) {
+
+                $scope.info = products;
+                $scope.message = "";
+            }
+            else {
+
+                $scope.info = null;
+                $scope.message = "No products to display";
+            }
+        }
+    }
+]);
+
